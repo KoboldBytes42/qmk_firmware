b26e2e33910f7eb44aade5343177e49463b3ac69
Clemens Zeiner <clemens@zeiner.io>
2025-07-10 16:26:32 +0200

    working+OLED_LEDs not working

--- keyboards/kobold/config.h
+++ keyboards/kobold/config.h
@@ -1,12 +1,26 @@
 #pragma once
 
+#include "quantum.h"
+
 /* USB Device descriptor parameter */
+#define USE_SERIAL
 #define MASTER_RIGHT
 
 /* key matrix size */
 // Rows are doubled-up
 #define MATRIX_ROWS 10
 #define MATRIX_COLS 6
+
+//enable LEDs
+//#define RGBLIGHT_SPLIT
+//#define WS2812_DI_PIN D3  //done in .json
+////#define RGBLIGHT_LED_COUNT 74
+//#define RGBLED_SPLIT {37,37}
+
+//OLED support
+#define OLED_DRIVER_ENABLE
+#define OLED_DISPLAY_128X32
+//#define SPLIT_OLED_ENABLE. //done in .json
 
 // wiring of each half
 #define MATRIX_ROW_PINS { C6, D7, E6, B4, B5 }
@@ -19,12 +33,5 @@
 #define ENCODER_A_PINS { F5 }
 #define ENCODER_B_PINS { F4 }
 #define ENCODER_A_PINS_RIGHT { F4 }
-#define ENCODERS_PAD_B_RIGHT { F5 }
+#define ENCODER_B_PINS_RIGHT { F5 }
 #define ENCODER_RESOLUTION 2
-
-
-/* communication between sides */
-
-//#define NO_ACTION_MACRO
-//#define NO_ACTION_FUNCTION
-

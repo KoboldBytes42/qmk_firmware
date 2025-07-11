7aa1375e950bfb59de9937971efdf72a42cb30b1
Clemens Zeiner <clemens@zeiner.io>
2025-07-11 22:47:09 +0200

    On the left side single LEDs are working, right order needs to be done

--- keyboards/kobold/config.h
+++ keyboards/kobold/config.h
@@ -5,16 +5,19 @@
 
 /* USB Device descriptor parameter */
 #define USE_SERIAL
-#define MASTER_RIGHT
+#define MASTER_LEFT
+
+//#define SPLIT_TRANSPORT_MIRROR
+#define ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
 
 /* key matrix size */
 // Rows are doubled-up
 //#define MATRIX_ROWS 10
-//#define MATRIX_COLS 6
+//#define MATRIX_COLS 6. -> done in .json//
 
 //enable LEDs
 //#define RGBLIGHT_SPLIT
-#define WS2812_DI_PIN D3  //done in .json
+//#define WS2812_DI_PIN D3  //done in .json
 //#define RGBLIGHT_LED_COUNT 74
 //#define RGBLED_SPLIT {37,37}
 
@@ -24,11 +27,10 @@
 //#define SPLIT_OLED_ENABLE. //done in .json
 
 // wiring of each half
-#define MATRIX_ROW_PINS { C6, D7, E6, B4, B5 }
-#define MATRIX_COL_PINS_RIGHT { B6, B2, B3, B1, F7, F6 }
-#define MATRIX_ROW_PINS_RIGHT { C6, D7, E6, B4, B5 }
-#define MATRIX_COL_PINS { F6, F7, B1, B3, B2, B6 }
-
+//#define MATRIX_ROW_PINS { C6, D7, E6, B4, B5 }
+//#define MATRIX_COL_PINS_RIGHT { B6, B2, B3, B1, F7, F6 }
+//#define MATRIX_ROW_PINS_RIGHT { C6, D7, E6, B4, B5 }
+//#define MATRIX_COL_PINS { F6, F7, B1, B3, B2, B6 }
 
 /* encoder support 
 #define ENCODER_A_PINS { F5 }
@@ -37,25 +39,3 @@
 #define ENCODER_B_PINS_RIGHT { F5 }
 #define ENCODER_RESOLUTION 2 */
 
-/*
-#define LAYOUT(L00, L01, L02, L03, L04, L05,           R00, R01, R02, R03, R04, R05,  \
-               L10, L11, L12, L13, L14, L15,           R10, R11, R12, R13, R14, R15,  \
-               L20, L21, L22, L23, L24, L25,           R20, R21, R22, R23, R24, R25,  \
-               L30, L31, L32, L33, L34, L35, L40, R40, R30, R31, R32, R33, R34, R35, \
-               L41, L42, L43, L44, L45, R41, R42, R43, R44, R45  \
-    ) \
-    { \
-        { L00, L01, L02, L03, L04, L05 }, \
-        { L10, L11, L12, L13, L14, L15 }, \
-        { L20, L21, L22, L23, L24, L25 }, \
-        { L30, L31, L32, L33, L34, L35 }, \
-        { L40, L41, L42, L43, L44, L45 }, \
-        { R05, R04, R03, R02, R01, R00 }, \
-        { R15, R14, R13, R12, R11, R10 }, \
-        { R25, R24, R23, R22, R21, R20 }, \
-        { R35, R34, R33, R32, R31, R30 }, \
-        { R45, R44, R43, R42, R41, R40 } \
-    }*/
-
-
-    
--- keyboards/kobold/rules.mk
+++ keyboards/kobold/rules.mk
@@ -1,7 +1,10 @@
 OLED_ENABLE = yes
-RGBLIGHT_ENABLE = yes
-RGBLIGHT_SPLIT = yes
-ENCODER_ENABLE = no
+#RGBLIGHT_ENABLE = yes
+#RGBLIGHT_SPLIT = yes
+RGB_MATRIX_ENABLE = yes
+#ENCODER_ENABLE = no
 VIA_ENABLE = yes
-SPLIT_KEYBOARD = yes
+#SPLIT_KEYBOARD = yes
 KEYMAP = samurai
+
+#all done in keyboarg.json now

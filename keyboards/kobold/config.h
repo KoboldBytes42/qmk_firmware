8fb5f66af98f921c26ba57b7381dfe8a419997f0
Clemens Zeiner <clemens@zeiner.io>
2026-04-21 22:56:42 +0200

    [Keyboard] Update Kobold configuration and keymap for RP2040 support

--- keyboards/kobold/config.h
+++ keyboards/kobold/config.h
@@ -1,7 +1,11 @@
 #pragma once
 
+// RP2040 Bootloader configuration
 
-//#include "quantum.h"
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U // Timeout window in ms in which the double tap can occur.
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17 // Specify a optional status led by GPIO number which blinks when entering the bootloader
+
 
 /* USB Device descriptor parameter */
 #define USE_SERIAL
@@ -13,36 +17,6 @@
 #define UNICODE_CYCLE_PERSIST true
 #define UNICODE_TYPE_DELAY 10
 
-
-//#define SPLIT_TRANSPORT_MIRROR
-//#define ENABLE_RGB_MATRIX_SOLID_REACTIVE
-
-/* key matrix size              -> done in .json */
-// Rows are doubled-up
-//#define MATRIX_ROWS 10
-//#define MATRIX_COLS 6. 
-
-//enable LEDs.                  //done in .json
-//#define RGBLIGHT_LED_COUNT 74
-//#define RGBLIGHT_SPLIT
-//#define WS2812_DI_PIN D3  
-//#define RGBLED_SPLIT {37,37}
-
 //OLED support
 #define OLED_DRIVER_ENABLE
 #define OLED_DISPLAY_128X32
-//#define SPLIT_OLED_ENABLE.    kukn//done in .json
-
-// wiring of each half
-//#define MATRIX_ROW_PINS { C6, D7, E6, B4, B5 }
-//#define MATRIX_COL_PINS_RIGHT { B6, B2, B3, B1, F7, F6 }
-//#define MATRIX_ROW_PINS_RIGHT { C6, D7, E6, B4, B5 }
-//#define MATRIX_COL_PINS { F6, F7, B1, B3, B2, B6 }
-
-/* encoder support 
-#define ENCODER_A_PINS { F5 }
-#define ENCODER_B_PINS { F4 }
-#define ENCODER_A_PINS_RIGHT { F4 }
-#define ENCODER_B_PINS_RIGHT { F5 }
-#define ENCODER_RESOLUTION 2 */
-
--- keyboards/kobold/rules.mk
+++ keyboards/kobold/rules.mk
@@ -1,13 +1,9 @@
+
 OLED_ENABLE = yes
-#RGBLIGHT_ENABLE = yes
-#RGBLIGHT_SPLIT = yes
 RGB_MATRIX_ENABLE = yes
-#ENCODER_ENABLE = no
 VIA_ENABLE = no
-#SPLIT_KEYBOARD = yes
 KEYMAP = samurai
 COMBO_ENABLE = yes
 UNICODE_COMMON = yes
-#UNICODEMAP_ENABLE = yes
 
-#all done in keyboarg.json now
+#all done in keyboard.json now

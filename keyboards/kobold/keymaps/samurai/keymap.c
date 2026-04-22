6a216ca0692282c00f2df3543a9b92350e9e4c08
Clemens Zeiner <clemens@zeiner.io>
2026-04-22 23:56:18 +0200

    enabled LEDs, OLEDs and some new Keymappings.

--- keyboards/kobold/keymaps/samurai/keymap.c
+++ keyboards/kobold/keymaps/samurai/keymap.c
@@ -84,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  | Bspc |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | CAPS  |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
+ * |   \  |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |. =.
  *   |
@@ -97,7 +97,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = LAYOUT(
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_MINUS,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
-  KC_CAPS,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
+  KC_BSLS,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, _______,     _______,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_EQL,
                  KC_LCTL,KC_LALT,KC_LGUI, MO(1), KC_ENT,            KC_SPC,  MO(2), KC_RGUI, KC_RALT, KC_RCTL
 ),
@@ -117,7 +117,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------'           '------''---------------------------'
  */
 [1] = LAYOUT(
-  _______, KC_F1,      KC_F2,   KC_F3,   KC_F4,   KC_F5,                          KC_F6,    KC_F7,      KC_F8,   KC_F9,      KC_F10,  KC_F11,
+  UC_NEXT, KC_F1,      KC_F2,   KC_F3,   KC_F4,   KC_F5,                          KC_F6,    KC_F7,      KC_F8,   KC_F9,      KC_F10,  KC_F11,
   KC_GRV,  UC_NEXT,    _______, _______, _______, _______,                        _______,  UC(0x00FC), _______, UC(0x00F6), _______, KC_F12,
   _______, UC(0x00E4), KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                        KC_CIRC,  KC_AMPR,    KC_LPRN, KC_RPRN,    KC_ASTR, KC_PIPE,
   _______, KC_EQL,     KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR, _______,      _______, _______,  KC_GRV,     KC_LBRC, KC_RBRC,    KC_BSLS, KC_BSLS,
@@ -125,9 +125,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 /* RAISE
  * ,----------------------------------------.                    ,-----------------------------------------.
- * |      |      |      |      |      |      |                    | ValU |      |ModNxt|      | SpdU | OnOff|
+ * |      |      |      |      |      |      |                    | ValU |ModPre|ModNxt|      | SpdU | OnOff|
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Esc  | Ins  | Pscr | Menu |      |      |                    | ValD | PagD |  Up  | HueD | SpdD | Del  |
+ * | Esc  | Ins  | Pscr | Menu |      |      |                    | ValD | PagD |  Up  | PagU | SpdD | Del  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Tab  | LAt  | LCtl |LShift|      | Caps |-------.    ,-------| SatU | Left | Down | Right| HueU | Bspc |
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
@@ -138,10 +138,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------'           '------''---------------------------'
  */
 [2] = LAYOUT(
-  _______, UG_VALD, UG_VALU, UG_SPDD, UG_SPDU, UG_TOGG,                          RM_VALU, _______, RM_NEXT, _______,  RM_SPDU, RM_TOGG,
-  _______, KC_INS,  KC_PSCR, KC_APP,  XXXXXXX, XXXXXXX,                          RM_VALD, KC_PGDN, KC_UP,   KC_PGUP,  RM_SPDD, KC_DEL,
-  _______, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX, KC_CAPS,                          RM_SATU, KC_LEFT, KC_DOWN, KC_RGHT,  RM_HUEU, KC_BSPC,
-  _______, KC_UNDO, KC_CUT, KC_COPY, KC_PASTE, XXXXXXX,  _______,       _______, RM_SATD, _______, _______, _______,  RM_HUED, KC_RSFT,
+  _______, UG_VALD, UG_VALU, UG_SPDD, UG_SPDU, UG_TOGG,                          RM_VALU, RM_PREV, RM_NEXT, _______,  RM_SPDU, RM_TOGG,
+  _______, KC_INS,  KC_PSCR, KC_APP,  _______, _______,                          RM_VALD, KC_PGDN, KC_UP,   KC_PGUP,  RM_SPDD, KC_DEL,
+  _______, KC_LALT, KC_LCTL, KC_LSFT, _______, KC_CAPS,                          RM_SATU, KC_LEFT, KC_DOWN, KC_RGHT,  RM_HUEU, KC_BSPC,
+  _______, KC_UNDO, KC_CUT, KC_COPY, KC_PASTE, _______,  _______,       _______, RM_SATD, _______, _______, _______,  RM_HUED, KC_RSFT,
                      _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______
 )
 };
